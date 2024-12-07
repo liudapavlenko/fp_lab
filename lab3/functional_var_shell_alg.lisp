2706f41b46ba8e435cc4f2f44b0257401d952bc0
liudapavlenko <147340981+liudapavlenko@users.noreply.github.com>
2024-12-07 02:50:43 +0200

    Update functional_var_shell_alg.lisp

--- lab3/functional_var_shell_alg.lisp
+++ lab3/functional_var_shell_alg.lisp
@@ -10,13 +10,13 @@ CL-USER> (defun shell-alg-loop2 (lst k i)
     (shell-alg-loop3 lst k i)
     (shell-alg-loop2 lst k (1+ i))))
 SHELL-ALG-LOOP2
-CL-USER> (defun shell-alg (lst k)
+CL-USER> (defun functional-shell-alg (lst k)
   (if k
       (progn
         (shell-alg-loop2 lst (car k) (car k))
-        (shell-alg  lst (cdr k)))
+        (functional-shell-alg  lst (cdr k)))
       lst))
-SHELL-ALG
-CL-USER> (shell-alg '(9 8 3 7 5 6 4 2 1) '(4 2 1))
+FUNCTIONAL-SHELL-ALG
+CL-USER> (functional-shell-alg '(9 8 3 7 5 6 4 2 1) '(4 2 1))
 (1 2 3 4 5 6 7 8 9)
 CL-USER> 
